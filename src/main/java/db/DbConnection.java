25c54e2cb036b7a9bf0c0c2782f450f522d4f6ff
“Thushara1020” <thusharabawantha2001@gmail.com>
2025-12-04 15:48:54 +0530

    Implement Singleton DbConnection and initialize MySQL connection

--- src/main/java/db/DbConnection.java
+++ src/main/java/db/DbConnection.java
@@ -1,4 +1,27 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbConnection {
+
+    private static DbConnection instance;
+
+    private Connection connection;
+
+    private DbConnection() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/thogakade","root","TB20010415");
+    }
+
+    public static DbConnection getInstance() throws SQLException {
+        if (instance == null){
+            instance = new DbConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
 }
