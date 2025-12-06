58d4591499d938453948974017c29c3441195beb
“Thushara1020” <thusharabawantha2001@gmail.com>
2025-12-06 11:15:53 +0530

    Refactor MedicinesFormController: rename actions, fix navigation, and update scene loading

--- src/main/java/controller/medicines/MedicinesFormController.java
+++ src/main/java/controller/medicines/MedicinesFormController.java
@@ -2,12 +2,18 @@ package controller.medicines;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MedicinesFormController {
+    Stage stage = new Stage();
 
     @FXML
     private Button btnAdd;
@@ -63,17 +69,32 @@ public class MedicinesFormController {
     }
 
     @FXML
-    void btnApdateAction(ActionEvent event) {
+    void btnUpdateAction(ActionEvent event) {
 
     }
 
     @FXML
     void btnBillingAction(ActionEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/billing.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Billing Form");
+        stage.show();
 
     }
 
     @FXML
     void btnDashBoardAction(ActionEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/dashboard.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Dashboard Form");
+        stage.show();
+
 
     }
 
@@ -84,16 +105,37 @@ public class MedicinesFormController {
 
     @FXML
     void btnLogoutAction(ActionEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/login.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Login Form");
+        stage.show();
 
     }
 
     @FXML
     void btnSalesAction(ActionEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/sales.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Sales Form");
+        stage.show();
 
     }
 
     @FXML
     void btnSuppliersAction(ActionEvent event) {
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/suppliers.fxml"))));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        stage.setTitle("Suppliers Form");
+        stage.show();
 
     }
 
