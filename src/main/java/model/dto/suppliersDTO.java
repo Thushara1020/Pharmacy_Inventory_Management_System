30fcd3ee5ce141d969586c69f155acedb8e42f2f
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-15 23:46:38 +0530

    implement suppliersDTO for vendor management

--- src/main/java/model/dto/suppliersDTO.java
+++ src/main/java/model/dto/suppliersDTO.java
@@ -1,4 +1,16 @@
 package model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class suppliersDTO {
+    private int id;
+    private String name;
+    private String contactNumber;
+    private String email;
+    private String address;
 }
