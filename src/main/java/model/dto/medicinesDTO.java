ef4a811917f39e1060a254e14c8515ba1383b359
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-15 23:45:56 +0530

    implement medicinesDTO with Lombok and LocalDate

--- src/main/java/model/dto/medicinesDTO.java
+++ src/main/java/model/dto/medicinesDTO.java
@@ -1,5 +1,20 @@
 package model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class medicinesDTO {
 
+    private int id;
+    private String name;
+    private String brand;
+    private LocalDate expiryDate;
+    private int quantity;
+    private double price;
 }
