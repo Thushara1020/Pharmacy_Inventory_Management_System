cf2bf9fea9e356990d7431a74c21da1ee0f5830f
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-15 23:44:54 +0530

    define BillingService interface with CRUD method signatures

--- src/main/java/controller/billing/BillingService.java
+++ src/main/java/controller/billing/BillingService.java
@@ -1,5 +1,16 @@
 package controller.billing;
 
+import javafx.beans.Observable;
+import javafx.collections.ObservableList;
+import model.dto.billingDTO;
+
 public interface BillingService{
 
+    void addBillingDetails(String itme, int qty, double Price, int total);
+
+    void deleteBillingDetails(String item);
+
+    void updateBillingDetails(String item, int qty, double Price, int total);
+
+    ObservableList<billingDTO> getAllBillingDetails();
 }
