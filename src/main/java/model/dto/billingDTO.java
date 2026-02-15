647b3046ef56c7e88882a59d8d2328b90aba5315
“Thushara1020” <thusharabawantha2001@gmail.com>
2026-02-15 23:45:36 +0530

    implement billingDTO with Lombok annotations

--- src/main/java/model/dto/billingDTO.java
+++ src/main/java/model/dto/billingDTO.java
@@ -1,5 +1,19 @@
 package model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class billingDTO {
 
+    private String item;
+
+    private int qty;
+
+    private double Price;
+
+    private int total;
 }
